6a96fefe073d2b66c033d2730a2c567ba36892f3
Samiramrullah <samiramrullah@gmail.com>
2026-04-19 23:30:35 +0530

    hero content changed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -62,12 +62,12 @@ export default function Hero() {
               </div>
 
               <div>
-                <h3 className="text-2xl font-semibold text-[#C8A75B]">100+</h3>
+                <h3 className="text-2xl font-semibold text-[#C8A75B]">1000+</h3>
                 <p className="text-sm text-gray-400">Hours Reachered</p>
               </div>
 
               <div>
-                <h3 className="text-2xl font-semibold text-[#C8A75B]">1000+</h3>
+                <h3 className="text-2xl font-semibold text-[#C8A75B]">2000+</h3>
                 <p className="text-sm text-gray-400">People Serviced</p>
               </div>
             </div>
